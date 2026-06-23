d17e1fc57cdfd5a3ccee2db73bb2353fd6f54d2e
Pietro <pb05129000@tutamail.com>
2026-06-24 00:51:44 +0200

    feat(infra): terraform backend config for dev and prod environments

---
+++ backend/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+  profile = "aparcar-management"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "aparcar-terraform-state-022079552075"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "aparcar-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
