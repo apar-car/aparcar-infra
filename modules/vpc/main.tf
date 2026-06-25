0114d23d7aae6650f7e6662eb1d59e93b07f9649
Pietro <pb05129000@tutamail.com>
2026-06-25 21:12:08 +0200

    fix: move s3 versioning resource to correct file

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -186,11 +186,3 @@ resource "aws_default_security_group" "main" {
     ManagedBy   = "terraform"
   }
 }
-
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
