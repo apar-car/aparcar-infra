523a3f3ceba037030186d625d5af4f0371e12b2e
Pietro <pb05129000@tutamail.com>
2026-06-25 21:08:06 +0200

    fix(security): checkov remediation - default sg, dynamodb pitr, s3 versioning

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -191,6 +191,6 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
 
   versioning_configuration {
-    status = enabled
+    status = "Enabled"
   }
 }
