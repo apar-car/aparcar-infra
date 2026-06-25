08424fdb30a244b5c8e606e4255f5c221e03bca3
Pietro <pb05129000@tutamail.com>
2026-06-25 21:10:48 +0200

    fix: move s3 versioning resource to correct file

--- backend/main.tf
+++ backend/main.tf
@@ -31,3 +31,11 @@ resource "aws_dynamodb_table" "terraform_locks" {
     prevent_destroy = true
   }
 }
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
