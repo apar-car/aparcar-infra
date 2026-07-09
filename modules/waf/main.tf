4351672765a24bbffec3bc3f76816c03e2d2a811
Pietro <pb05129000@tutamail.com>
2026-07-09 16:19:10 +0200

    fix(waf): increase WAF log retention to 365 days

--- modules/waf/main.tf
+++ modules/waf/main.tf
@@ -93,7 +93,7 @@ resource "aws_wafv2_web_acl_association" "appsync" {
 
 resource "aws_cloudwatch_log_group" "waf" {
   name              = "aws-waf-logs-${var.project}-${var.environment}"
-  retention_in_days = 30
+  retention_in_days = 365
 
   tags = {
     Environment = var.environment
